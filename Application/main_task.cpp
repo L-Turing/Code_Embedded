cbd34ce516a61d0302109e0e3e6934bd7c830d0c
L <LC90623224@outlook.com>
2026-08-09 04:14:27 +0800

    测试

--- Application/main_task.cpp
+++ Application/main_task.cpp
@@ -6,6 +6,7 @@
 #include "cmsis_os.h"
 #include "main.h"
 #include "string.h"
+#include "usbd_cdc_if.h"
 
 //全局变量
 SD_LogBlock block;
@@ -15,6 +16,16 @@ void StartImuAcquire(void * argument)
   (void)argument;
   SignalMark(type_signal::LED_G_ON);
   for (;;) {
+    SignalMark(type_signal::LED_R_Breathe_ON);
+    SignalMark(type_signal::LED_G_ON);
+    SignalMark(type_signal::LED_1_ON);
+    SignalMark(type_signal::LED_2_ON);
+    SignalMark(type_signal::LED_3_ON);
+    SignalMark(type_signal::LED_4_ON);
+    SignalMark(type_signal::LED_5_ON);
+    SignalMark(type_signal::LED_6_ON);
+    SignalMark(type_signal::LED_7_ON);
+    SignalMark(type_signal::LED_8_ON);
     osDelay(1);
   }
 }
@@ -30,17 +41,26 @@ void StartFaultMonitor(void * argument)
 void StartStorage(void * argument)
 {
   (void)argument;
-  //SD_Mount();
-  for (;;) {
-    // memset(&block, 0, sizeof(block));
-    // block.seq = 1;
-    // block.timestamp_us = DWT_GetTimeline_ms();
-    // block.type = 0x1;                      // 示例类型
-    // block.length = 8;                      // 示例长度
-    // memcpy(block.payload, "TestData", 8);  // 示例数据
-    // SD_LogWriteBlock(&block);
+  SD_Mount();
 
-    //SD_Unmount();
-    osDelay(2);
+  // --- CDC_Transmit_FS 测试 ---
+  uint8_t cdc_test_buf[64];
+  snprintf(
+    (char *)cdc_test_buf, sizeof(cdc_test_buf), "CDC Test OK! Tick: %lu\r\n",
+    (unsigned long)DWT_GetTimeline_ms());
+  uint8_t cdc_result = CDC_Transmit_FS(cdc_test_buf, strlen((char *)cdc_test_buf));
+  // cdc_result: USBD_OK (0) = 成功, USBD_BUSY = 忙, USBD_FAIL = 失败
+
+  for (;;) {
+    memset(&block, 0, sizeof(block));
+    block.seq = 1;
+    block.timestamp_us = DWT_GetTimeline_ms();
+    block.type = 0x1;                      // 示例类型
+    block.length = 8;                      // 示例长度
+    memcpy(block.payload, "TestData", 8);  // 示例数据
+    SD_LogWriteBlock(&block);
+
+    SD_Unmount();
+    osDelay(3000);
   }
 }
